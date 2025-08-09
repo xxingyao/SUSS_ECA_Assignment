768b1521c5297d053a897b7a7a07bd60b3a56c13
Lau Xing Yao <lauxingyao4188@gmail.com>
2025-08-09 16:31:53 +0800

    changed start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,5 @@
-export FLASK_APP=app.py; export PYTHONPATH=.; export FLASK_DEBUG=1;
-flask run --host=0.0.0.0
+#!/usr/bin/env bash
+export FLASK_APP=app.py
+export PYTHONPATH=.
+export FLASK_DEBUG=1
+flask run --host=0.0.0.0 --port=$PORT
